f838fa3305fa2695ff72626654ab348dbbfc917e
Luxary <f.leiton001@gmail.com>
2025-01-10 11:31:34 -0300

    fix post, added delete and patch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const books = require("./src/books.json");
 const { validateBook, partialValidateBook } = require("./book.js");
 const Crypto = require("crypto");
 const cors = require("cors");
+const fs = require("node:fs");
 
 const app = express();
 
@@ -55,8 +56,56 @@ app.post("/books", (req, res) => {
     id: Crypto.randomUUID(),
     ...result.data,
   };
+  /// agrega el nuevo libro al array de libros pero no lo modifica en el .json
   books.push(newBook);
+  ///sobreescribe el archivo .json con la nueva array de libros, con el nuevo incluido
+  fs.writeFileSync("./src/books.json", JSON.stringify(books));
   res.status(201).json(newBook);
+});
+
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+
+  const indice = books.findIndex((b) => b.id === id);
+
+  if (indice === -1) {
+    return res.status(404).json({
+      error: `no se encotro ningun recurso con el id: ${id}`,
+    });
+  }
+
+  const deletedBook = books[indice];
+  books.splice(indice, 1);
+  fs.writeFileSync("./src/books.json", JSON.stringify(books));
+  return res.status(200).json(deletedBook);
+});
+
+app.patch("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const indice = books.findIndex((b) => b.id === id);
+
+  if (indice === -1) {
+    return res.status(404).json({
+      error: `no se encontro ningun recurso con el id: ${id}`,
+    });
+  }
+
+  const result = partialValidateBook(req.body);
+
+  if (result.error) {
+    return res.status(400).json(JSON.parse(result.error.message));
+  }
+
+  const patchedBook = {
+    ...books[indice],
+    ...result.data,
+  };
+
+  books[indice] = patchedBook;
+
+  fs.writeFileSync("./src/books.json", JSON.stringify(books));
+
+  return res.status(200).json(patchedBook);
 });
 
 const PORT = 4000;
